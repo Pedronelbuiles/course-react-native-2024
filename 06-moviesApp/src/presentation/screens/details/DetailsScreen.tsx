11364763c7903ca75618c51b2578c06a3a21fe8c
Pedro Nel Builes Moreno <pedronel.builes@gmail.com>
2024-05-27 17:17:14 -0500

    finish app

--- 06-moviesApp/src/presentation/screens/details/DetailsScreen.tsx
+++ 06-moviesApp/src/presentation/screens/details/DetailsScreen.tsx
@@ -1,11 +1,11 @@
 import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
-import {Text} from 'react-native';
 import {RootStaackParams} from '../../navigation/Navigation';
 import {UseMovie} from '../../hooks/UseMovie';
 import {MovieHeader} from '../../components/movie/MovieHeader';
 import {MovieDetails} from '../../components/movie/MovieDetails';
 import {ScrollView} from 'react-native-gesture-handler';
+import {FullScreenLoader} from '../../components/loaders/FullScreenLoader';
 
 interface Props extends StackScreenProps<RootStaackParams, 'Details'> {}
 
@@ -13,10 +13,9 @@ export const DetailsScreen = ({route}: Props) => {
   // const {movieId} = useRoute().params;
   const {movieId} = route.params;
   const {isLoading, movie, castState = []} = UseMovie(movieId);
-  console.log(movie);
 
   if (isLoading) {
-    return <Text>Cargando...</Text>;
+    return <FullScreenLoader />;
   }
   return (
     <ScrollView>
