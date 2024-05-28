96d2739fb797d204dd522bfc6ed66f0a67097354
Pedro Nel Builes Moreno <pedronel.builes@gmail.com>
2024-05-28 10:20:42 -0500

    addance project 07

---
+++ 07-componentsApp/index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+import 'react-native-gesture-handler';
+
+import {AppRegistry} from 'react-native';
+import {name as appName} from './app.json';
+import {ComponentsApp} from './src/ComponentsApp';
+
+AppRegistry.registerComponent(appName, () => ComponentsApp);
---
+++ 07-componentsApp/src/ComponentsApp.tsx
@@ -0,0 +1,11 @@
+import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
+import {Navigator} from './presentation/navigator/Navigator';
+
+export const ComponentsApp = () => {
+  return (
+    <NavigationContainer>
+      <Navigator />
+    </NavigationContainer>
+  );
+};
